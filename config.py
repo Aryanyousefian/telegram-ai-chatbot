c93ee4a98768e2e997073aa85da9d6fb803541eb
Aryanyousefian <103969380+Aryanyousefian@users.noreply.github.com>
2026-09-06 18:15:16 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+TELEGRAM_TOKEN = os.getenv(
+    "TELEGRAM_TOKEN"
+)
+
+GEMINI_API_KEY = os.getenv(
+    "GEMINI_API_KEY"
+)
+
+DEEPSEEK_API_KEY = os.getenv(
+    "DEEPSEEK_API_KEY"
+)
