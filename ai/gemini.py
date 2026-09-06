247cbd00338693c51c73df05ac40301603b40d2e
Aryanyousefian <103969380+Aryanyousefian@users.noreply.github.com>
2026-09-06 18:15:55 +0330

    Update gemini.py

--- ai/gemini.py
+++ ai/gemini.py
@@ -1,1 +1,20 @@
-# Gemini AI
+import os
+import google.generativeai as genai
+
+
+genai.configure(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+
+async def ask_gemini(text):
+
+    model = genai.GenerativeModel(
+        "gemini-2.0-flash"
+    )
+
+    response = model.generate_content(
+        text
+    )
+
+    return response.text
